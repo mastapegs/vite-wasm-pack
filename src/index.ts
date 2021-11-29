9917e30eb44818c51dd3a91d82d04be2532f245f
Matthew Pagan <mastapegs01@gmail.com>
2021-11-29 13:45:11 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+//@ts-ignore
+import init, { greet } from 'my-crate';
+// Don't worry if vscode told you can't find my-crate
+// It's because you're using a local crate
+// after yarn dev, wasm-pack plugin will install my-crate for you
+
+init().then(() => {
+  console.log('init wasm-pack');
+  greet('from vite!');
+});
