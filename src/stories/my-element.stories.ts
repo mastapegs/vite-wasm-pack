01dd4bc298fb45bfb47b70c197d7d3556a0766d4
Matthew Pagan <mastapegs01@gmail.com>
2021-11-29 16:48:36 -0500

    fix: use TS types in stories

--- src/stories/my-element.stories.ts
+++ src/stories/my-element.stories.ts
@@ -1,13 +1,17 @@
-import { Story, Meta } from '@storybook/web-components';
-import { html } from 'lit-html';
-import '../my-element';
+import { MyElement } from './../my-element';
+import { Story, Meta } from "@storybook/web-components";
+import { html } from "lit-html";
+
+import '../my-element'
 
 // More on default export: https://storybook.js.org/docs/web-components/writing-stories/introduction#default-export
 export default {
-  title: 'my-element',
+  title: "my-element",
+  component: 'my-element',
+  argTypes: { onClick: { action: "clicked" } },
 } as Meta;
 
 // More on component templates: https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
-const Template: Story = () => html`<my-element></my-element>`;
+const Template: Story<MyElement> = () => html`<my-element></my-element>`;
 
 export const Primary = Template.bind({});
